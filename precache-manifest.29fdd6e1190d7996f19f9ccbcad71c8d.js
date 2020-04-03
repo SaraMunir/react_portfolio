bdb69ded46b4332bf73f38b5e735e742176c4438
Sara Munir <saramunir011@gmail.com>
2020-04-03 00:03:52 -0400

    Updates

---
+++ precache-manifest.29fdd6e1190d7996f19f9ccbcad71c8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f15693454eb656ff666b7305edd60fe",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "6cd8180d57dbc0b08f0b",
+    "url": "/react_portfolio/static/css/main.040ea27a.chunk.css"
+  },
+  {
+    "revision": "ee633cb6e7b492ebc2fd",
+    "url": "/react_portfolio/static/js/2.c8b3f671.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_portfolio/static/js/2.c8b3f671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd8180d57dbc0b08f0b",
+    "url": "/react_portfolio/static/js/main.53fe4658.chunk.js"
+  },
+  {
+    "revision": "6d7b0fea43cde4224782",
+    "url": "/react_portfolio/static/js/runtime-main.e9d5f759.js"
+  },
+  {
+    "revision": "32160a40af4f66a9c7a54b0f146340a4",
+    "url": "/react_portfolio/static/media/backgroundPortfolio.32160a40.png"
+  }
+]);
