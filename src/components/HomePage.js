47782d18330acd57d55b5e5ac2377b81abf6c567
Sara Munir <saramunir011@gmail.com>
2020-04-02 23:57:41 -0400

    updated navBar

---
+++ src/components/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+function HomePage() {
+    // const aboutStyle= {
+    //     height: '100vh',
+    //     backgroundImage: 'url("./assets/backgroundPortfolio.png");',
+    //     backgroundSize: 'cover',
+    //     backgroundPostition: 'center' 
+    // }
+    return (
+        <div class="jumbotron jumbotron-fluid hero" >
+            <div class="container text-right hero1" >
+                <h1 class="display-4 heroHead" >Sara Munir</h1>
+                {/* <p class="lead ">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p> */}
+            </div>
+        </div>
+
+    )
+}
+
+export default HomePage
---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link, useLocation } from "react-router-dom";
+
+function NavBar( props ) {
+    const location = useLocation();
+
+    return (
+        <div class="container-fluid navParent ">
+            <nav class="navBar">
+                <ul class="d-flex justify-content-around">
+                    <li className="nav-item">
+                        <Link to="/about" className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>
+                        About
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/ProjectPage" className={location.pathname === "/ProjectPage" ? "nav-link active" : "nav-link"}>
+                        Project
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/ContactPage" className={location.pathname === "/ContactPage" ? "nav-link active" : "nav-link"}>
+                        Contact Me
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
