1292382a14548ed316b3f14808997bcd53b39416
Sara Munir <saramunir011@gmail.com>
2020-04-06 23:06:42 -0400

    updated contacts page

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import ScrollAnimation from 'react-animate-on-scroll';
-import groupImg from './assets/43069.jpg';
+import mapImg from './assets/torontoMap.jpg';
 function ContactPage() {
+    const imgStyle= {width: "100%", height: "80vh", objectFit: "cover"}
     return (
         <div>
             <div class="bodyContained">
                 <ScrollAnimation animateIn="fadeIn" animateOut='fadeOut'>
-                    <div class="row container mx-auto myBox">
-                        <div class="col-lg-6">
-                            <img src={groupImg} alt="weather app" style={{width: "100%"}}/>
-                        </div>
-                        <div class="col-lg-6 text-center pt-4">
-                            <h2 class="mt-3">
-                            Fitness Tracker
-                            </h2>
-                            <p>Fitness Diaries is a full-stack web application that uses MVC design pattern with local server-side API. The application is designed for people who are conscious about their health and follow a fitness regime or lose weight with a socializing aspect. The app addresses the need of having some friends around who can motivate and cheer you up in your fitness journey instead of a showcasing a plain list of exercises and diet plans. It's concise compared to social networking channels and broader than chat applications.
-                            </p>
-                            <div class="d-flex justify-content-center mt-4">
-                                <a class="myBtn text-center" href="https://fitness-diaries.herokuapp.com/">LIVE DEMO</a>
+                    <div class="container myBox">
+                        <div class="row mx-auto myMy ">
+                            <img class="col-lg-6" src={mapImg} alt="weather app" style={imgStyle}/>
+                            <div class="col-lg-6 text-center contactMe">
+                                <h2 class="mt-3">Contact Me
+                                </h2>
+                                <form class="card-body mt-4">
+                                    <div class="form-group text-left">
+                                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter Your Name"/>
+                                    </div>
+                                    <div class="form-group  text-left">
+                                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Email Address"/>
+                                    </div>
+                                    <div class="form-group text-left">
+                                        <label for="exampleFormControlTextarea1">Enter your Message</label>
+                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                    </div>
+                                    <div class="d-flex justify-content-center mt-5">
+                                        <a class="myBtn text-center" href="https://fitness-diaries.herokuapp.com/">SUBMIT</a>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
